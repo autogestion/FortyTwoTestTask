aa12fbb3ac73043287911a252c5ea999acb6a544
Kushchenko Mykhailo <nozdre@gmail.com>
2014-07-08 14:07:24 +0300

    ticket1: test failed

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# class Contact(models.Model):
+#     first_name = models.CharField(verbose_name="Name", max_length=100)
+#     last_name = models.CharField(verbose_name="Last name", max_length=100)
+#     birthday = models.DateField("Birthday")
+#     bio = models.TextField(verbose_name="Bio")
+#     email = models.EmailField(verbose_name="Email")
+#     jabber = models.CharField(verbose_name="Jabber", max_length=100)
+#     skype = models.CharField(verbose_name="Skype", max_length=100)
+#     other_contacts = models.TextField(verbose_name="Other contacts")
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,15 @@
-from django.test import TestCase
 
-# Create your tests here.
-class SomeTests(TestCase):
-    def test_math(self):
-        assert(2+2==5)
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.test.client import Client
+from .models import Contact
+
+
+class HttpTest(TestCase):
+    def test_home(self):
+        myself = Contact.objects.get(id=1)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, myself.first_name)
+        self.assertContains(response, myself.last_name)
+        self.assertContains(response, myself.skype)
