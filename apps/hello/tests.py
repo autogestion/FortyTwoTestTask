6e78a258b2206b0c29a49aee3297a5f934a8ccae
Kushchenko Mykhailo <nozdre@gmail.com>
2014-07-08 15:49:05 +0300

    ticket3: test failed

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -2,10 +2,14 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
-from .models import Contact
+from django.test.client import RequestFactory
+
+from .models import Contact, HttpRequestList
+from .middleware import SaveAllHttpRequests
+from .views import requestList
 
 
-class HttpTest(TestCase):
+class ContactTest(TestCase):
     def test_home(self):
         myself = Contact.objects.all()[0]
         response = self.client.get(reverse('home'))
@@ -13,3 +17,16 @@ class HttpTest(TestCase):
         self.assertContains(response, myself.first_name)
         self.assertContains(response, myself.last_name)
         self.assertContains(response, myself.skype)
+
+
+
+class MiddlewareTest(TestCase):
+     def test_requests_middleware(self):
+         self.factory = RequestFactory()
+         request = self.factory.get(reverse('requestList'))
+         response = requestList(request)
+         self.assertEqual(response.status_code, 200)
+         md = SaveAllHttpRequests()
+         count = HttpRequestList.objects.all().count()
+         md.process_request(request)
+         self.assertNotEqual(HttpRequestList.objects.all().count(), count)
